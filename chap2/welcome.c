8f6764f586d9b90b8a1493266bd972c9004945d2
Sai Charla <lakshmisaicharla@gmail.com>
2017-10-02 10:48:03 +0530

    Listing 2.1 : A Simple C Program.

---
+++ chap2/welcome.c
@@ -0,0 +1,15 @@
+/*
+Program: Welcome.c
+Author: JGarrido
+Description: Display 'Welcome C world on the screen and value of the variable.
+*/
+
+#include <stdio.h>
+
+int main(){
+  double x; // variable declaration
+  printf("Welcome to the C World \n");
+  x = 49.45; // assign a value to a variable
+  printf("value of x: %f \n", x);
+  return 0;
+}
