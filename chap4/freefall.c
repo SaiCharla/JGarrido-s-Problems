5120d1ab216597a5d463e8fcedc132d387926837
SaiCharla <lakshmisaicharla@gmail.com>
2017-10-16 20:47:38 +0530

    Listing 4.1: Computaional model of a freefalling body in C.

--- chap4/freefall.c
+++ chap4/freefall.c
@@ -10,11 +10,13 @@ Description: print vertical position and velocity at agiven time in SI units.
 int main(void) {
   const double g = 9.81;    //accleration due to gravity
   const double Y_0 = 40.0;   // Initial height
-  double y, vel, t;    // vertical position, velocity anf time
+  double y;
+  double vel;
+  double t;
   t = 1.2;
   y = Y_0 - 0.5 * (g*t*t);
   vel = -g*t;
-  printf("Vertical position at time %lf is %lf\n" t, y);
-  printf("Vertical position at time %lf is %lf\n" t, vel);
+  printf("Vertical position at time %lf is %lf\n", t, y);
+  printf("Vertical position at time %lf is %lf\n", t, vel);
   return 0;
 }
