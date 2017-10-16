771003dc17b8067bc1ace4ea7bcf40c5daef0dfa
Sai Charla <lakshmisaichrala@gmail.com>
2017-10-16 10:20:10 +0530

    Listing 4.1 Free falling object program in C

--- chap4/freefall.c
+++ chap4/freefall.c
@@ -7,3 +7,14 @@ Description: print vertical position and velocity at agiven time in SI units.
 
 #include<stdio.h>
 
+int main(void) {
+  const double g = 9.81;    //accleration due to gravity
+  const double Y_0 = 40.0;   // Initial height
+  double y, vel, t;    // vertical position, velocity anf time
+  t = 1.2;
+  y = Y_0 - 0.5 * (g*t*t);
+  vel = -g*t;
+  printf("Vertical position at time %lf is %lf\n" t, y);
+  printf("Vertical position at time %lf is %lf\n" t, vel);
+  return 0;
+}
